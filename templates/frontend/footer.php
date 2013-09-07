5d75b810eb4b6507c8db624f51848f9a9134dc91
AlfioEmanueleFresta <alfio.emanuele.f@gmail.com>
2013-09-07 12:17:52 +0100

    Added boostrap 3.0

---
+++ templates/frontend/footer.php
@@ -0,0 +1,13 @@
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2013</p>
+      </footer>
+    </div>
+
+    <script src="../../assets/js/jquery.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
